cb118b8796e4df4db2814e5ee169d131831353aa
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-04-16 08:10:00 +0530

    test: do not upload to IPFS in test env

--- src/utils/make-uploader-function.ts
+++ src/utils/make-uploader-function.ts
@@ -1,16 +1,26 @@
+import { randomBytes } from 'crypto'
 import { getUrlForIPFSHash, uploadToIPFS } from './ipfs'
 import { Handler, Operation } from './make-api'
+
+const IS_TEST = process.env.NODE_ENV === 'test'
 
 async function makeUploaderFunction<T extends Operation>(operation: T) {
 	const handler: Handler<T> = async(req) => {
 		const createdAt = new Date()
 		// add additional metadata to the application
 		const fullData = { ...req, createdAt }
-		// stringify the grant & push to IPFS
-		const { hash } = await uploadToIPFS(
-			JSON.stringify(fullData), 
-			{ contentType: 'application/json', filename: `qb-${operation}-${createdAt.getTime()}.json` }
-		)
+
+		let hash: string
+		if(IS_TEST) {
+			hash = randomBytes(8).toString('hex')
+		} else {
+			// stringify the grant & push to IPFS
+			const result = await uploadToIPFS(
+				JSON.stringify(fullData), 
+				{ contentType: 'application/json', filename: `qb-${operation}-${createdAt.getTime()}.json` }
+			)
+			hash = result.hash
+		}
 
 		return {
 			ipfsHash: hash,
