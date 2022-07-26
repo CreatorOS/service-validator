bbfc7bf142b7c72bd9a8a4ccd7bb5642b827d299
Angel <angel333gomez@gmail.com>
2022-07-25 23:53:00 -0400

    linted

--- src/tests/test.token-fetch.ts
+++ src/tests/test.token-fetch.ts
@@ -1,12 +1,10 @@
-import request from 'supertest'
 import fetchTokenPriceFunction from '../routes/fetchTokenPrice'
-import { Response } from '../utils/make-api'
 import { describeWithApp } from './test-setup'
 
 describeWithApp('Token Tests', app => {
 	it('fetches token price', async() => {
-        const fetch = await fetchTokenPriceFunction('FRA');
+		const fetch = await fetchTokenPriceFunction('FRA')
 
-        expect(fetch.data.data.quote).toBeDefined();
+		expect(fetch.data.data.quote).toBeDefined()
 	})
 })
